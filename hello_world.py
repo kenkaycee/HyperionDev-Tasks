8bae6fdc32badf470b70b18787421ddf24926f9d
kenkaycee <36804752+kenkaycee@users.noreply.github.com>
2024-12-17 10:13:24 +0000

    Submitting Task 1
    Create a new Python file in your folder for this task, and call it
    hello_world.py.
    ● First, provide pseudocode as comments in your Python file, outlining
    how you will solve this problem (you’ll need to read the rest of this
    practical task first, of course).
    ● Now, inside your hello_world.py file, write Python code to take in a user’s
    name using input() and then print out (output) the name.
    ● Use the same input and output approach to take in a user’s age and
    print it out.
    ● Finally, print the string “Hello World!” on a new line. Note: The new line
    will happen by default if you use a separate print statement to the one
    you used immediately above to print out the age. This is because each
    print statement automatically inserts an “enter”, or newline instruction,
    at the end

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+# TASK 1
+
+
+# request user to input their name 
+name = input("What is your name? ")
+
+# output the user name 
+print(f"Your name is {name}")
+
+# request user to input their age 
+age = int(input("How old are you? "))
+# output the age inputted by the user 
+print(f"You are {age} years old")
+
+# print "Hello World!" using print()
+print("Hello World!")
