c9d47a3e7977e2939cf2f61199c291c7f3c584e2
kenkaycee <36804752+kenkaycee@users.noreply.github.com>
2024-12-17 10:14:21 +0000

    Task 2 - Converstion of data types

---
+++ conversion.py
@@ -0,0 +1,18 @@
+# declare variables 
+num1 = 99.23 
+num2 = 23
+num3 = 150 
+string1 = "100"
+
+# convert "num1" to an integer using int()
+num1 = int(num1)
+# convert "num2" into a float using float()
+num2 = float(num2)
+# convert num3 into a string using str()
+num3 = str(num3)
+# convert "string1" into an integer using int()
+string1 = int(string1)
+
+# print all the converted variables 
+# use "\n" to print the variables on a seperate line
+print(f"{num1} \n{num2} \n{num3} \n{string1}")
